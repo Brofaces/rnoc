e158766e81865c4a17f73848683c5b955765bba9
Andrew Butcher <abutcher@afrolegs.com>
2012-08-25 01:47:42 -0400

    Inflate rails stuff.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rnoc::Application.config.secret_token = '955b38bc37e1e93be9647e2ac9cf845c5a39303e4bcc83397477bcf3fd5d93a05ad8d5560fd3f98b57cca748514a02c7c617488d34876f451f200705a1eb219b'
