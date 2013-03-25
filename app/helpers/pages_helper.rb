051d4645d1dd034c4a307b54778e2da89ca8ba08
Tim Bielawa <tbielawa@redhat.com>
2013-03-24 21:10:20 -0400

    Defuse SSL pageboxes. No more explosions, yay

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,11 +6,6 @@ module PagesHelper
     if item.downcase.match('(png|jpg|gif)$')
       return "<img src='#{item}'></img>"
     else
-      uri_item = URI.parse(URI.encode(item))
-      http = Net::HTTP.new(uri_item.host, uri_item.port)
-      request = Net::HTTP::Get.new(uri_item.request_uri)
-
-      response = http.request(request)
       return "<iframe src='#{item}'></iframe>"
     end
   end
