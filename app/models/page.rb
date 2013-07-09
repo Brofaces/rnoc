e311f6af918af2968a6175b84a3c390ab180580d
Andrew Butcher <abutcher@redhat.com>
2013-07-09 14:48:02 -0400

    HOTFIX: CHANGE TO TICKS.

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,7 @@ class Page < ActiveRecord::Base
   validates_presence_of :title, :order
 
   def next
-    @next = Page.where('"pages"."order" > :order', {:order => self.order}).where(:enabled => true).sort_by{|page| page[:order]}.first
+    @next = Page.where('`pages`.`order` > :order', {:order => self.order}).where(:enabled => true).sort_by{|page| page[:order]}.first
     if @next == nil
       @next = Page.where(:enabled => true).sort_by{|page| page[:order]}.first
     end
@@ -11,7 +11,7 @@ class Page < ActiveRecord::Base
   end
 
   def prev
-    @prev = Page.where('"pages"."order" < :order', {:order => self.order}).where(:enabled => true).sort_by{|page| page[:order]}.last
+    @prev = Page.where('`pages`.`order` < :order', {:order => self.order}).where(:enabled => true).sort_by{|page| page[:order]}.last
     if @prev == nil
       @prev = Page.where(:enabled => true).sort_by{|page| page[:order]}.last
     end
